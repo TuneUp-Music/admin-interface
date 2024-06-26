52b1d796bcd3360a16624705e5d4ce99e2eab552
STARBOY <julien.espes@epitech.eu>
2024-06-26 18:31:41 +0900

    [ADD] ag grid style

--- src/app/Components/Posts/posts.component.ts
+++ src/app/Components/Posts/posts.component.ts
@@ -64,7 +64,7 @@ export class PostsComponent implements OnInit {
 
   generateColDefs(): void {
     this.colDefs = this.PostKeys.map((key, value) => ({
-      checkboxSelection: key === "_	id" ? true : false,
+      checkboxSelection: key === "_id" ? true : false,
       field: key as string,
       headerName: this.formatHeaderName(key),
     }));
